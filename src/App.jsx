efaa566a41a3c5ac96521e6b0d6c2307280f1235
mjamecny <jamecny.m@tutanota.com>
2023-07-17 16:02:25 +0200

    the react quizz app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter"
+import Content from "./Content"
+import Header from "./Header"
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Content>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Content>
     </div>
   )
 }
